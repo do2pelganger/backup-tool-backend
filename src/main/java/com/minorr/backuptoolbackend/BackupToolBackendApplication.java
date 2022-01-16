80cedf8fb08612846734bb2663cd9aa9f52070c3
sashaminorr <sashaminorr@protonmail.com>
2022-01-16 20:25:53 +0200

    init commit

---
+++ src/main/java/com/minorr/backuptoolbackend/BackupToolBackendApplication.java
@@ -0,0 +1,13 @@
+package com.minorr.backuptoolbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackupToolBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackupToolBackendApplication.class, args);
+	}
+
+}
