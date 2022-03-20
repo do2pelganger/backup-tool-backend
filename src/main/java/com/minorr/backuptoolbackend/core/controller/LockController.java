b4be39586e14a9210f54829a4447aceafc63a849
sashaminorr <sashaminorr@protonmail.com>
2022-03-20 23:44:47 +0200

    Added todo

--- src/main/java/com/minorr/backuptoolbackend/core/controller/LockController.java
+++ src/main/java/com/minorr/backuptoolbackend/core/controller/LockController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 
 @Controller
 public class LockController {
+    // take this from the DB as hash
     public final String password = "test";
 
     public LockController(){
