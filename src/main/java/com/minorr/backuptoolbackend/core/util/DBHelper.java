a63854f09436335699c91d7a8d3dc61197a1b6a0
sashaminorr <sashaminorr@protonmail.com>
2022-03-21 06:38:29 +0200

    Added: DBHelper class

---
+++ src/main/java/com/minorr/backuptoolbackend/core/util/DBHelper.java
@@ -0,0 +1,51 @@
+package com.minorr.backuptoolbackend.core.util;
+
+import java.sql.*;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+import com.minorr.backuptoolbackend.core.config.DatabaseConfiguration;
+import com.minorr.backuptoolbackend.core.config.StorageConfiguration;
+
+@Component
+@Scope("singleton")
+public class DBHelper {  
+
+    private Connection conn;
+
+    public DBHelper(){
+        try {
+            this.createTables();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());  
+        }
+    }
+
+    private String getConnectionUrl(){
+        return "jdbc:sqlite:" + StorageConfiguration.HOME + DatabaseConfiguration.DB_NAME;  
+    }
+    
+    private Boolean createTables(){
+        try {
+            for(String ddl : DatabaseConfiguration.DDL_CREATE){
+                PreparedStatement ps = this.getConnection().prepareStatement(ddl);
+                ps.executeUpdate();
+            }
+            return true;
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());  
+            return false;
+        }
+    }
+   
+    public Connection getConnection() {  
+        if(this.conn == null){
+            try {  
+                this.conn = DriverManager.getConnection(getConnectionUrl());  
+            } catch (SQLException e) {  
+                System.out.println(e.getMessage());  
+                return null;
+            }  
+        }
+        return this.conn;
+    }  
+}  
