4bda059db84d1731ee65fb4622c80d509e27d851
sashaminorr <sashaminorr@protonmail.com>
2022-03-21 06:36:31 +0200

    Added: StorageConfiguration class

---
+++ src/main/java/com/minorr/backuptoolbackend/core/config/StorageConfiguration.java
@@ -0,0 +1,8 @@
+package com.minorr.backuptoolbackend.core.config;
+
+public class StorageConfiguration {
+    public static final String HOME = System.getProperty("user.dir") + "\\";
+    public static final String STORAGE = HOME + "storage" + "\\";
+
+    public static final String ENCRYPTED_EXT = ".wnbck";
+}
