8cd432054522e0c2dbea7bdd42e7e0849f87fc99
sashaminorr <sashaminorr@protonmail.com>
2022-04-09 16:37:45 +0300

    Updated backup model

--- src/main/java/com/minorr/backuptoolbackend/core/model/Backup.java
+++ src/main/java/com/minorr/backuptoolbackend/core/model/Backup.java
@@ -7,17 +7,31 @@ public class Backup {
     private UUID id;
     private String name;
     private String comment;
+    private String path;
     private Long created;
     private Long size;
     private Integer compressionMethod;
     private Integer encryptionMethod;
     private Boolean isCustomPassword;
 
-    public Backup(String name, Long size, String comment, Integer compressionMethod, Integer encryptionMethod, Boolean isCustomPassword){
-        this.id = UUID.randomUUID();
+    public Backup(UUID id, String name, Long size, String comment, String path, Integer compressionMethod, Integer encryptionMethod, Boolean isCustomPassword){
+        this.id = id;
         this.name = name;
         this.comment = comment;
+        this.path = path;
         this.created = Instant.now().getEpochSecond();
+        this.size = size;
+        this.compressionMethod = compressionMethod;
+        this.encryptionMethod = encryptionMethod;
+        this.isCustomPassword = isCustomPassword;
+    }
+
+    public Backup(UUID id, String name, Long size, String comment, String path, Long created, Integer compressionMethod, Integer encryptionMethod, Boolean isCustomPassword){
+        this.id = id;
+        this.name = name;
+        this.comment = comment;
+        this.path = path;
+        this.created = created;
         this.size = size;
         this.compressionMethod = compressionMethod;
         this.encryptionMethod = encryptionMethod;
@@ -32,6 +46,9 @@ public class Backup {
     }
     public String getComment(){
         return comment;
+    }
+    public String getPath(){
+        return path;
     }
     public Long getCreated(){
         return created;
@@ -49,5 +66,7 @@ public class Backup {
         return isCustomPassword;
     }
 
-    
+    public void setSize(Long size){
+        this.size = size;
+    }
 }
