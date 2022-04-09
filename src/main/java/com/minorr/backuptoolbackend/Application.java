6a7d10ee4c89d4412210343730b0b91cb2b5f57f
sashaminorr <sashaminorr@protonmail.com>
2022-04-09 16:33:10 +0300

    Excluded SecurityAutoConfiguration for testing purposes

--- src/main/java/com/minorr/backuptoolbackend/Application.java
+++ src/main/java/com/minorr/backuptoolbackend/Application.java
@@ -2,8 +2,11 @@ package com.minorr.backuptoolbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
+// @SpringBootApplication
+
 public class Application {
 
 	public static void main(String[] args) {
