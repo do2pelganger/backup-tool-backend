dfb2d4a3e8070a2d352be5fed9ec74ac13d6574b
sashaminorr <sashaminorr@protonmail.com>
2022-04-09 16:39:15 +0300

    Refactored LockRestController

--- src/main/java/com/minorr/backuptoolbackend/web/controller/LockRestController.java
+++ src/main/java/com/minorr/backuptoolbackend/web/controller/LockRestController.java
@@ -2,12 +2,15 @@ package com.minorr.backuptoolbackend.web.controller;
 
 import java.util.Map;
 
-import com.minorr.backuptoolbackend.core.controller.LockController;
+import javax.validation.Valid;
+
+import com.minorr.backuptoolbackend.core.controller.SettingsController;
 import com.minorr.backuptoolbackend.web.config.Configuration;
 import com.minorr.backuptoolbackend.web.config.MessageTemplates;
 import com.minorr.backuptoolbackend.web.model.request.UnlockRequest;
 import com.minorr.backuptoolbackend.web.model.response.BasicResponse;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -22,11 +25,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/unlock")
 @CrossOrigin(origins = Configuration.FRONT_URL)
 public class LockRestController {
-    @RequestMapping(method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
-    public BasicResponse unlock(@RequestBody UnlockRequest unlockRequest){
-        LockController lockController = new LockController();
+    private SettingsController settingsController;
+
+    @Autowired
+    public LockRestController(SettingsController settingsController) {
+        this.settingsController = settingsController;
+    }
+
+    @RequestMapping(method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+    public BasicResponse unlock(@Valid @RequestBody UnlockRequest unlockRequest){
         System.out.println(unlockRequest.getPassword());
-        if(lockController.isCorrect(unlockRequest.getPassword())){
+        if(this.settingsController.isMasterPassCorrect(unlockRequest.getPassword())){
             BasicResponse response = new BasicResponse(HttpStatus.OK, MessageTemplates.NO_MSG);
             return response;
         }
