2623aab8084108dbccb9b6a774442b47e070d7ee
sashaminorr <sashaminorr@protonmail.com>
2022-04-09 16:38:58 +0300

    Updated BackupBuilder

--- src/main/java/com/minorr/backuptoolbackend/core/util/BackupBuilder.java
+++ src/main/java/com/minorr/backuptoolbackend/core/util/BackupBuilder.java
@@ -1,23 +1,40 @@
 package com.minorr.backuptoolbackend.core.util;
 
+import java.util.UUID;
+
 import com.minorr.backuptoolbackend.core.model.Backup;
 
 public class BackupBuilder {
     
+    private UUID id;
     private String name;
     private String comment;
+    private String path;
+    private Long created;
     private Long size;
     private Integer compressionMethod;
     private Integer encryptionMethod;
     private Boolean isCustomPassword;
-    
+
+    public void setId(String id){
+        this.id = UUID.fromString(id);
+    }
+    public void generateUUID(){
+        this.id = UUID.randomUUID();
+    }
     public void setName(String name){
         this.name = name;
     }
     public void setComment(String comment){
         this.comment = comment;
     }
-    public void setSize(long size){
+    public void setPath(String path){
+        this.path = path;
+    }
+    public void setCreated(Long created){
+        this.created = created;
+    }
+    public void setSize(Long size){
         this.size = size;
     }
     public void setCompressionMethod(Integer compressionMethod){
@@ -31,8 +48,11 @@ public class BackupBuilder {
     }
 
     public void reset(){
+        this.id = null;
         this.name = null;
         this.comment = null;
+        this.path = null;
+        this.created = null;
         this.size = null;
         this.compressionMethod = null;
         this.encryptionMethod = null;
@@ -40,6 +60,11 @@ public class BackupBuilder {
     }
 
     public Backup build(){
-        return new Backup(name, size, comment, compressionMethod, encryptionMethod, isCustomPassword);
+        if(created == null){
+            // new backup
+            return new Backup(id, name, size, comment, path, compressionMethod, encryptionMethod, isCustomPassword);
+        }
+        // existing backup
+        return new Backup(id, name, size, comment, path, created, compressionMethod, encryptionMethod, isCustomPassword);
     }
 }
